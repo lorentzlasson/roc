257fd2ea80478c488aa6f8a4cfd8da43ecd08ab4
Agus Zubiaga <hi@aguz.me>
2023-11-29 19:04:58 -0300

    Parse and format inline import with exposed names

--- crates/compiler/parse/src/ast.rs
+++ crates/compiler/parse/src/ast.rs
@@ -455,6 +455,7 @@ pub enum ValueDef<'a> {
     ModuleImport {
         name: Loc<crate::header::ModuleName<'a>>,
         alias: Option<Loc<crate::header::ModuleName<'a>>>,
+        exposed: Collection<'a, Loc<Spaced<'a, crate::header::ExposedName<'a>>>>,
     },
 }
 
@@ -1791,9 +1792,11 @@ impl<'a> Malformed for ValueDef<'a> {
                 condition,
                 preceding_comment: _,
             } => condition.is_malformed(),
-            ValueDef::ModuleImport { name, alias } => {
-                name.value.contains_dot() || alias.map_or(false, |x| x.value.contains_dot())
-            }
+            ValueDef::ModuleImport {
+                name,
+                alias,
+                exposed: _,
+            } => name.value.contains_dot() || alias.map_or(false, |x| x.value.contains_dot()),
         }
     }
 }
